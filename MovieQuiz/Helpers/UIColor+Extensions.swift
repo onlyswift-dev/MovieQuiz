347aa11d41d1c7742457c8d2ba5f5915bf7c3d61
kaiot <Psi12@me.com>
2025-09-16 13:19:47 +0300

    Подготовка проекта к сдаче

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+     var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+     var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+     var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+     var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+     var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+     var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+}
